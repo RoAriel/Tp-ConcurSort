3e2b7bd7fccd60708cbe882d6f39b4cfb92c48fa
Gonzalez <ariel_gonzalez06@yahoo.com.ar>
2015-06-07 21:57:41 -0300

    Se creo la clase Counter

---
+++ src/main/java/quick/sort/list/Counter.java
@@ -0,0 +1,32 @@
+package quick.sort.list;
+
+public class Counter {
+
+	Integer cont;
+	Boolean isZero;
+
+	public Counter(Integer initial) {
+		this.cont = initial;
+		this.isZero = false;
+	}
+
+	public synchronized void dec() {
+		if (this.isZero) {
+			notifyAll();
+		}
+
+		this.cont--;
+
+	}
+
+	public synchronized void waitZero() {
+
+		if (!this.isZero)
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+	}
+}
